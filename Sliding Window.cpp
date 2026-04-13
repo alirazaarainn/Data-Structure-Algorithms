081ec3c05880345b59455b64dbfdc0ce06ad7972
alirazaarainn <alirazaarain547@gmail.com>
2026-04-13 15:42:51 +0500

    Modified

--- Sliding Window.cpp
+++ Sliding Window.cpp
@@ -3,14 +3,26 @@ using namespace std;
 
 int main (){
 
-    int arr [2, 4,6,8,10,12];
+    int arr [] = {2, 4, 6, 8, 10, 12};
     int n = 6;
-    int k = 3;
+    int w = 3;
 
-    int sum =0;
-    for(int i=0; i < k; i++){
+    int sum = 0;
+
+    for(int i = 0; i < w; i++){
         sum += arr[i];
     }
 
+    int maxSum = sum;
+
+    for(int i = w; i < n; i++){
+        sum = sum + arr[i] - arr[i - w];
+
+        if(sum > maxSum){
+            maxSum = sum;
+        }
+    }
+
+    cout << "Maximum Sum = " << maxSum;
 
 }
